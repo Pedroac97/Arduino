c5b4521e7b7f53fc1b73a6b2295a51f33ac37636
Pedro A Cordeiro <pedro_a_c_@hotmail.com>
2021-04-08 19:15:33 -0400

    Ajustes V2.0
    Ajustes de botões e funções.

--- Testes/Capacete/Capacete.ino
+++ Testes/Capacete/Capacete.ino
@@ -1,23 +1,32 @@
-#define Visor 12
-#define BTN 5
+//Servo Motor
+#include <Servo.h>
+Servo myservo;
 
-int pressionado = 0;
+#define Visor 12
+#define BTN1 5
+#define BTN2 4
+
+int pressionado1 = 0;
+int pressionado2 = 0;
 int modo = 0;
 
-//--------------------------------------<<
+//------------------------------------------------<< setup
 void setup(){
   Serial.begin(9600); 
   pinMode(Visor, OUTPUT);
-    pinMode(BTN, INPUT);
+  pinMode(BTN1, INPUT);
+  pinMode(BTN2, INPUT);
+  
+  //Servo
+   myservo.attach(13); 
 }
 
-//------------------------------------------<<
+//------------------------------------------------<< loop
 void loop(){
   Serial.println(modo);
   Serial.println("--------");
-  pressionado = digitalRead(BTN);
-  Serial.println(modo);
-  Serial.println("--------");
+  pressionado1 = digitalRead(BTN1);
+  pressionado2 = digitalRead(BTN2);
   
   botao();
   
@@ -25,38 +34,54 @@ void loop(){
     Ligado();
   }
   if(modo == 1){
+    Fechar();
+  }
+  if(modo == 2){
     Abrir();
   }
 }
-//------------------------------------------------<<
+//------------------------------------------------<< função
 // função capacete
 void Ligado(){
-    delay(100);
-    digitalWrite(Visor, HIGH);   // Visor ligado
-    //myservo.write(0); // posição do Servo motor   
+  delay(100);
+  digitalWrite(Visor, HIGH);   // Visor ligado
+  myservo.write(0); // posição do Servo motor   
 }
+
 void Abrir(){
-    delay(100);
-    digitalWrite(Visor, LOW);   // Visor ligado
-    //myservo.write(85); // posição do Servo motor   
+  delay(100);
+  digitalWrite(Visor, LOW);   // Visor ligado
+  myservo.write(85); // posição do Servo motor   
 }
 
-
+void Fechar(){
+  myservo.write(0); // posição do Servo motor
+  delay(100);
+  digitalWrite(Visor, HIGH);   // Visor ligado    
+}
 
 // função Botão
 void botao(){
-  if(pressionado == HIGH){
-    if(modo == 0){
-      modo += 1;
-      Serial.println(modo);
-      Serial.println("--------");
-      delay(150); // tempo para coreção de botão. problemas com conato
-    }
-    else{
-      modo -= 1;
-      Serial.println(modo);
-      Serial.println("--------");
-      delay(150); // tempo para coreção de botão.
-    }
+  if(pressionado1 == HIGH){
+      if(modo == 0){
+        modo += 2;
+        Serial.println(modo);
+        Serial.println("--------");
+        delay(200); // tempo para coreção de botão. problemas com conato
+      }
+      if(modo == 1){
+        modo += 1;
+        Serial.println(modo);
+        Serial.println("--------");
+        delay(200); // tempo para coreção de botão.
+      }
+  }
+  if(pressionado2 == HIGH){
+      if(modo == 2){
+        modo -= 1;
+        Serial.println(modo);
+        Serial.println("--------");
+        delay(200); // tempo para coreção de botão.
+      }
   }
 }
